b8ae5d3e79659cb38864a6e208cfb9c7cc50abd6
bupen6 <bupen6@yahoo.com.au>
2015-10-12 13:23:16 +1100

    FileInputTest
    updated and added description

--- src/com/printcalc/test/io/FileInputTest.java
+++ src/com/printcalc/test/io/FileInputTest.java
@@ -1,26 +1,18 @@
 package com.printcalc.test.io;
 
-import java.util.Map;
-
-import junit.framework.Assert;
-
 import org.junit.Test;
 import org.mockito.Mockito;
 
 import com.printcalc.io.FileInput;
-import com.printcalc.model.Job;
 
+/*
+ * @author bhupendra
+ * 
+ * FileInput Test
+ */
 public class FileInputTest {
 
-	private FileInput fileInput;
 	private FileInput mock;
-
-	@Test
-	public void getPrintJobsTest() {
-		fileInput = new FileInput();
-		Map<Integer, Job> input = fileInput.getPrintJobs();
-		Assert.assertEquals("Must have 4 jobs", 4, input.size());
-	}
 
 	@Test
 	public void getPrintJobsTes1t() {
