d0a91a8e96c40bb26a7725ae0394a0f5a6eeb432
bupen6 <bupen6@yahoo.com.au>
2015-10-12 12:35:23 +1100

    PrintCaclConstants
    Constants for application

---
+++ src/com/printcalc/constants/PrintCalcConstants.java
@@ -0,0 +1,10 @@
+package com.printcalc.constants;
+
+
+public interface PrintCalcConstants {
+	public String DRIVER_TYPE = "com.mysql.jdbc.Driver"; //Database driver
+	public String CONNECTION_HOST = "jdbc:mysql://localhost:3306/printcalc"; //host url
+	public String user = "root"; //database user id
+	public String pass = "bhu@123!"; //database password
+	public String tableName = "pricingrules";
+}
