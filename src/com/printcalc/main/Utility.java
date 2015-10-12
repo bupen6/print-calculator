0241537fbc5667a552c061c07907c24cf457be93
bupen6 <bupen6@yahoo.com.au>
2015-10-12 12:40:02 +1100

    main class
    Class to run the application

---
+++ src/com/printcalc/main/Utility.java
@@ -0,0 +1,15 @@
+package com.printcalc.main;
+
+import com.printcalc.manager.CalculationManager;
+
+public class Utility {
+
+	/**
+	 * @author bhupendra
+	 */
+	public static void main(String[] args) {
+		CalculationManager cm = new CalculationManager();
+		cm.calculatePrice();
+	}
+
+}
